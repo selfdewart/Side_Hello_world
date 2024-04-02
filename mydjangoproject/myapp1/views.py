e9341c43badfe5253bf74586917ee18e78a95a73
selfDewArt <jamaldinovaidar@gmail.com>
2024-04-02 06:53:32 +0600

    обновленный список работников

--- mydjangoproject/myapp1/views.py
+++ mydjangoproject/myapp1/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from myapp1.models import Worker
+
 
 def index_page(request):
-    return render(request, 'index.html')
+
+    my_dictionary = {'data': 'lists', 'data2': 'hi'}
+
+    all_workers = Worker.objects.all()
+
+    return render(request, 'index.html', {'data': all_workers})
 
 def about(request):
     return render(request, 'about.html')
